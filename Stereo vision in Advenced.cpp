3fe9c9688b8719c5bdf72056b1a03221d5b63085
Acro748 <acro748@gmail.com>
2019-04-14 02:57:00 +0900

    depth_map.txt input and processing writing...

--- Stereo vision in Advenced.cpp
+++ Stereo vision in Advenced.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <opencv2\opencv.hpp>
 #include <opencv2\highgui.hpp>
 #include <opencv2\calib3d.hpp>
@@ -6,7 +7,51 @@
 using namespace std;
 using namespace cv;
 
-Mat leftimg, rightimg;
+/*depth_map.txt 가공*/
+void depth_map_processing()
+{
+	char processing[1080][1920]; //FHD 기준
+	char tmp;
+
+	/*depth_map.txt input*/
+	ifstream findep("depth_map/depth_map.txt");
+	if (!findep) {
+		cerr << "Cannot open depth_map.txt for read." << endl;
+		return;
+	}
+
+	for (int i = 0; i < 1080; i++) {
+		for (int j = 0; j < 1920; j++) {
+			findep >> tmp;
+			if (tmp <= 255 && 0 < tmp) findep >> processing[i][j];
+			else if (processing[i][j] == ';') break;
+			else i = i - 1;
+		}
+	}
+
+	/*depth_map_processing.txt output*/
+	ofstream foutpro("depth_map/depth_map_processing.txt");
+	if (!foutpro) {
+		cerr << "Cannot open depth_map_processing.txt for write." << endl;
+		return;
+	}
+}
+
+/*Depth_map 텍스트 파일 출력*/
+void depth_map_stream(Mat disp8)
+{
+	/*depth_map.txt output*/
+	ofstream foutdep("depth_map/depth_map.txt");
+	if (!foutdep) {
+		cerr << "Cannot open depth_map.txt for write." << endl;
+		return;
+	}
+
+	foutdep << disp8;
+	
+	/*depth_map.txt 가공*/
+	//depth_map_processing();
+}
 
 /*Depth_map 생성 메소드*/
 void depth_map(Mat leftimg, Mat rightimg)
@@ -38,7 +83,10 @@ void depth_map(Mat leftimg, Mat rightimg)
 	normalize(disp_left, disp8, 0, 255, CV_MINMAX, CV_8U);
 
 	/*Depth_map 이미지 작성*/
-	imwrite("image/depth_map.png", disp8);
+	imwrite("depth_map/depth_map.png", disp8);
+
+	/*Depth_map 텍스트 출력*/
+	depth_map_stream(disp8);
 
 	/*Depth_map 이미지 출력*/
 	namedWindow("depth map", WINDOW_AUTOSIZE);
@@ -51,6 +99,8 @@ void depth_map(Mat leftimg, Mat rightimg)
 
 int main()
 {
+	Mat leftimg, rightimg;
+
 	leftimg = imread("image/leftimg2.jpg", IMREAD_COLOR); //왼측 카메라
 	rightimg = imread("image/rightimg2.jpg", IMREAD_COLOR); //우측 카메라
 
